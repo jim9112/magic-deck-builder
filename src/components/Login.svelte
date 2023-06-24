ee6cdecf263cf3f1ba6ef1692c4813ad62f39d45
James Hannan <jim9112@gmail.com>
2023-06-24 15:01:57 -0400

    build register form

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -1,7 +1,13 @@
 <form action="">
-    <label for="email">Email</label>
-    <input type="email" name="email" id="email">
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password">
+    <div class="flex">
+        <div class="flex flex-col">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email">
+        </div>
+        <div class="flex flex-col">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+        </div>
+    </div>
     <input type="submit" value="Login">
 </form>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,12 @@
-<script>
+<script lang="ts">
   import Login from "../../components/Login.svelte";
-
+  import Register from "../../components/Register.svelte";
+    let login:Boolean = true;
 </script>
 <h1>Login</h1>
+{#if login} 
 <Login />
+{:else}
+<Register />
+{/if}
+<button on:click={()=>login = !login} type="button">Login</button>
