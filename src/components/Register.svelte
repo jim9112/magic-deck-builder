8873093abfc56903edbd124dc306a9890e8cc08c
James Hannan <jim9112@gmail.com>
2023-06-24 18:42:08 -0400

    create register logic

--- src/components/Register.svelte
+++ src/components/Register.svelte
@@ -1,38 +1,50 @@
 <script lang="ts">
   import CardLarge from "./global/CardLarge.svelte";
+  import {createUserWithEmailAndPassword} from'firebase/auth';
+  import {firebaseAuth} from '$lib/firebase';
     let email: string;
-    let userName: string;
     let password: string;
     let confirmPassword: string;
 
+    let passwordsMatch: boolean | undefined = undefined;
     const register = () => {
-        console.log('registered')
+        if (password === confirmPassword) {
+            passwordsMatch = true;
+            createUserWithEmailAndPassword(firebaseAuth, email, password).then((userCredenttial) => {
+                console.log(userCredenttial);
+            }).catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+            })
+        } else {
+            passwordsMatch = false
+        }
     }
 </script>
 <CardLarge>
     <form action="" on:submit|preventDefault={register}>
+        {#if passwordsMatch === false}
+        <span class="text-flame">Passwords do not match!!</span>
+        {/if}
         <div class="flex gap-4">
             <div class="flex flex-col">
                 <label for="email">Email</label>
                 <input bind:value={email} type="email" name="email" id="email">
             </div>
-            <div class="flex flex-col">
-                <label for="userName">User Name</label>
-                <input bind:value={userName} type="text" name="userName" id="userName">
-            </div>
-        </div>
-        <div class="flex gap-4">
             <div class="flex">
                 <div class="flex flex-col">
                     <label for="password">Password</label>
                     <input bind:value={password} type="password" name="password" id="password">
                 </div>
             </div>
+        </div>
+        <div class="flex gap-4">
             <div class="flex flex-col">
                 <label for="confirmPassword">Confirm Password</label>
                 <input bind:value={confirmPassword} type="password" name="confirmPassword" id="confirmPassword">
             </div>
+            <input type="submit" value="Sign Up">
         </div>
-        <input type="submit" value="Sign Up">
     </form>
 </CardLarge>
